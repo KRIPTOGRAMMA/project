f89d47a432384eed9a8ee4494c22d3482c4a816c
Talgat <bekmuhantalgat@gmail.com>
2026-03-23 19:39:40 +0500

    feat: done with project model

--- backend/src/models/project.py
+++ backend/src/models/project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Boolean, DateTime, text, ForeignKey
+from sqlalchemy import String, Boolean, DateTime, text, ForeignKey, func
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from src.core.db import Base
@@ -9,14 +9,22 @@ from src.models.workspace import Workspace
 class Project(Base):
     __tablename__ = 'projects'
 
-    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True,)
+    id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text('gen_random_uuid()')
+    )
 
-    workspace_id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True),)
-    workspace: Mapped[Workspace] = relationship('Workspace', back_populates='')
+    workspace_id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        ForeignKey('workspaces.id', ondelete='CASCADE'),
+        nullable=False    
+    )
+    workspace: Mapped[Workspace] = relationship('Workspace', back_populates='projects')
 
-    name: Mapped[str] = mapped_column(String(255))
-    description: Mapped[str] = mapped_column()
-    color: Mapped[str] = mapped_column()
-    is_archived: Mapped[bool] = mapped_column(Boolean)
-    created_at: Mapped[datetime] = mapped_column(DateTime())
-    updated_at: Mapped[datetime] = mapped_column(DateTime())
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(String(255))
+    color: Mapped[str] = mapped_column(String(7), server_default=text("#3B82F6"))
+    is_archived: Mapped[bool] = mapped_column(Boolean, server_default=text('false'))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), server_onupdate=func.now() ,nullable=False)
